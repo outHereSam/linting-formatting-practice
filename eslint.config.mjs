c39b4d98c3f0ed8352c614e2e72d7861160fc87f
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-12 09:03:05 +0000

    Installs prettier and configures prettier to work with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
@@ -21,4 +22,5 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
